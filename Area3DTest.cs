b7674e65d32de3185dbca9ada6a33f82f9123671
TheHyper-Dev <32967925+TheHyper-Dev@users.noreply.github.com>
2024-05-09 15:32:27 +0300

    Create Area3DTest.cs

---
+++ Area3DTest.cs
@@ -0,0 +1,24 @@
+using System;
+using Godot;
+
+[GlobalClass]
+public sealed partial class Area3DTest : SimpleArea3D
+{
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+    }
+    public override void _ExitTree()
+    {
+        base._ExitTree();
+    }
+    public override void OnBodyEnter(in CollisionObject3D collisionObject)
+    {
+        GD.Print($"object {collisionObject.Name} has just entered");
+    }
+
+    public override void OnBodyExit(in CollisionObject3D collisionObject)
+    {
+        GD.Print($"object {collisionObject.Name} has just exit");
+    }
+}
